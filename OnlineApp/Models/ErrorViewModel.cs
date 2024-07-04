db5f4c35bebd1262cc00e60b14fb2fbd1eec756e
Jonty <Jonty@DESKTOP-9AU6OLA>
2024-07-04 10:45:25 -0400

    Add project files.

---
+++ OnlineApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OnlineApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
